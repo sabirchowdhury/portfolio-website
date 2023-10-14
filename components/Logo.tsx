4c409dd3f176370209ca37c745492f0334f0de03
Sabir Chowdhury <sabir@wondersofmaths.com>
2023-10-14 18:27:34 +0100

    responsiveness

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -2,9 +2,9 @@ import { Terminal } from "@mui/icons-material";
 
 export default function Logo() {
   return (
-    <span className="flex items-center text-3xl">
+    <span className="flex items-center text-2xl md:text-3xl">
       <Terminal fontSize="inherit" />
-      <h1 className="font-extrabold ml-3 mr-2">Sabir</h1>{" "}
+      <h1 className="font-extrabold md:ml-3 md:mr-2 mr-1 ml-2">Sabir</h1>{" "}
       <h1 className="font-light">Chowdhury</h1>
     </span>
   );
