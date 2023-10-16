0cb9bacfa669b907b221ebf04c091aba1246f636
Sabir Chowdhury <sabir@wondersofmaths.com>
2023-10-16 18:07:32 +0100

    simplify bg

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -18,9 +18,10 @@ export default function Layout({ children }: { children: ReactNode }) {
         <Switcher />
       </div>
       {children}
-      <div className="absolute h-1/4 w-1/5 left-0 top-1/2 rounded-full bg-gradient-radial from-blue-100 to-transparent blur-2xl dark:bg-gradient-to-br dark:from-transparent dark:to-blue-700 dark:opacity-10 -z-20"></div>
-      <div className="absolute h-1/5 w-1/6 right-0 top-3/4 rounded-full bg-gradient-radial from-green-100 to-transparent blur-2xl dark:bg-gradient-to-br dark:from-transparent dark:to-red-700 dark:opacity-10 -z-20"></div>
-      <div className="absolute h-1/5 w-1/6 right-0 top-0 rounded-full bg-gradient-radial from-yellow-50 to-transparent blur-2xl dark:bg-gradient-to-br dark:from-transparent dark:to-green-700 dark:opacity-10 -z-20"></div>
+      <div className="absolute h-1/4 w-1/5 left-0 top-1/2 rounded-full bg-gradient-radial from-rose-950 to-transparent blur-2xl dark:bg-gradient-to-br dark:from-transparent dark:to-blue-700 dark:opacity-20 opacity-20 -z-20"></div>
+      <div className="absolute h-1/5 w-1/6 right-3/4 top-3/4 rounded-full bg-gradient-radial from-emerald-950 to-transparent blur-2xl dark:bg-gradient-to-br dark:from-transparent dark:to-red-700 dark:opacity-20 opacity-30 -z-20"></div>
+      <div className="absolute h-1/5 w-1/6 right-0 top-0 rounded-full bg-gradient-radial from-yellow-50 to-transparent blur-2xl dark:bg-gradient-to-br dark:from-transparent dark:to-orange-800 dark:opacity-20 -z-20"></div>
+      <div className="absolute h-1/2 w-1/3 right-1/4 bottom-1/4 rounded-full bg-gradient-radial from-sky-300 to-transparent blur-2xl dark:bg-gradient-to-br dark:from-transparent dark:to-green-700 dark:opacity-20 opacity-20 -z-20"></div>
     </div>
   );
 }
